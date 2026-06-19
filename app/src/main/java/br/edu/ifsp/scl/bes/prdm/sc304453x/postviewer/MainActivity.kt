c0b1961a3bf99d613243d6f5a206bcb20a9bc3d4
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-06-18 21:47:57 -0300

    feat: persist local comments

--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/postviewer/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/postviewer/MainActivity.kt
@@ -3,6 +3,8 @@ package br.edu.ifsp.scl.bes.prdm.sc304453x.postviewer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.room.Room
+import br.edu.ifsp.scl.bes.prdm.sc304453x.postviewer.data.local.AppDatabase
 import br.edu.ifsp.scl.bes.prdm.sc304453x.postviewer.data.remote.RetrofitClient
 import br.edu.ifsp.scl.bes.prdm.sc304453x.postviewer.data.repository.PostRepository
 import br.edu.ifsp.scl.bes.prdm.sc304453x.postviewer.ui.navigation.AppNavigation
@@ -12,8 +14,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val database = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java,
+            "postviewer.db"
+        ).build()
+
         val repository = PostRepository(
-            postApi = RetrofitClient.postApi
+            postApi = RetrofitClient.postApi,
+            localCommentDao = database.localCommentDao()
         )
 
         setContent {
--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/postviewer/data/repository/PostRepository.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/postviewer/data/repository/PostRepository.kt
@@ -1,11 +1,16 @@
 package br.edu.ifsp.scl.bes.prdm.sc304453x.postviewer.data.repository
 
+import br.edu.ifsp.scl.bes.prdm.sc304453x.postviewer.data.local.LocalCommentDao
+import br.edu.ifsp.scl.bes.prdm.sc304453x.postviewer.data.local.LocalCommentEntity
 import br.edu.ifsp.scl.bes.prdm.sc304453x.postviewer.data.remote.PostApi
 import br.edu.ifsp.scl.bes.prdm.sc304453x.postviewer.domain.model.Comment
 import br.edu.ifsp.scl.bes.prdm.sc304453x.postviewer.domain.model.Post
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class PostRepository(
-    private val postApi: PostApi
+    private val postApi: PostApi,
+    private val localCommentDao: LocalCommentDao
 ) {
     suspend fun getPosts(): List<Post> {
         return postApi.getPosts().map { postDto ->
@@ -28,5 +33,34 @@ class PostRepository(
                 isLocal = false
             )
         }
+    }
+
+    fun observeLocalCommentsByPostId(postId: Int): Flow<List<Comment>> {
+        return localCommentDao.observeCommentsByPostId(postId).map { localComments ->
+            localComments.map { localComment ->
+                Comment(
+                    id = localComment.id,
+                    postId = localComment.postId,
+                    name = localComment.name,
+                    email = localComment.email,
+                    body = localComment.body,
+                    isLocal = true
+                )
+            }
+        }
+    }
+
+    suspend fun addLocalComment(
+        postId: Int,
+        body: String
+    ) {
+        localCommentDao.insertComment(
+            LocalCommentEntity(
+                postId = postId,
+                name = "Comentário local",
+                email = "local@postviewer.com",
+                body = body
+            )
+        )
     }
 }
